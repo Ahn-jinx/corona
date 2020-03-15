e5ea987b501122c1f607d7c81d65c5a69e05f121
jinx <zibbork@gmail.com>
2020-03-15 17:25:19 +0900

    Updates

---
+++ precache-manifest.6f680d52ed2076e3fb967767cde018f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "971ac83218237a1467abca1b9cba5483",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53d96146e76e098180af",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "0c96ec61ea312963d33a",
+    "url": "/static/js/2.a3dcb2ba.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.a3dcb2ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53d96146e76e098180af",
+    "url": "/static/js/main.522db17d.chunk.js"
+  },
+  {
+    "revision": "1be0f4dc1dc12a62d439",
+    "url": "/static/js/runtime-main.c0038a1a.js"
+  }
+]);
